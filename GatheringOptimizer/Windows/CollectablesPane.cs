0c44b53c3d15df9e66796754f7ca8644f2aed897
HappySprite <belzaru17@gmail.com>
2024-07-23 03:20:21 +0100

    Initial version of collectaables window.

---
+++ GatheringOptimizer/Windows/CollectablesPane.cs
@@ -0,0 +1,52 @@
+﻿using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using ImGuiNET;
+
+namespace GatheringOptimizer.Windows;
+
+internal class CollectablesPane : IPane
+{
+    public string AddonName => "GatheringMasterpiece";
+    public string Title => "Collectables";
+
+    public void DrawPane()
+    {
+        ImGui.Text("Coming soon!");
+        ImGui.InputInt("GP", ref currentGP);
+    }
+
+    public void SetupFromAddon(AddonEvent type, AddonArgs args)
+    {
+    }
+
+    public bool ShouldAutoOpen()
+    {
+        return true;
+    }
+
+    public bool UpdateFromAddon(AddonEvent type, AddonArgs args)
+    {
+        if (Plugin.ClientState.LocalPlayer != null)
+        {
+            currentGP = (int)Plugin.ClientState.LocalPlayer.CurrentGp;
+        }
+
+        return true;
+    }
+
+    private unsafe void Debug()
+    {
+        var addon = (AddonGatheringMasterpiece*)Plugin.GameGui.GetAddonByName(AddonName);
+        if (addon == null)
+        {
+            Plugin.Log.Information("addon is null");
+            return;
+        }
+        Plugin.Log.Information($"Integrity: {addon->IntegrityLeftover->NodeText}");
+        var text = AddonUtils.GetTextNode(addon->GetTextNodeById(47));
+        Plugin.Log.Information($"Collectability: {text}");
+    }
+
+    private int currentGP;
+}
